a68a0e9e940ea34250533a4e2aacec18217387bc
SamuelPapyan <samuel.papyan170@gmail.com>
2024-02-10 14:12:17 +0400

    changed configurations

--- srcs/requirements/wordpress/conf/user.sh
+++ srcs/requirements/wordpress/conf/user.sh
@@ -1,4 +1,4 @@
-wp core install --url=localhost --title="my insception" --admin_user=${WP_ADMIN_USR} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL}
+wp core install --url=localhost --title="spapyan | inception" --admin_user=${WP_ADMIN_USR} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL}
 if wp user get Johnik >/dev/null 2>&1; then
     echo "User Johnik exists."
 else
@@ -10,5 +10,7 @@ if wp user get "spapyan" >/dev/null 2>&1; then
 else
     echo "Login failed. Please check your root credentials."
 fi
-wp theme activate twentytwentytwo
+wp theme install astra --activate
+wp theme install https://downloads.brainstormforce.com/astra/astra.zip --active
+wp theme install https://github.com/brainstormforce/astra/archive/master.zip --activate
 /usr/sbin/php-fpm8 -F
